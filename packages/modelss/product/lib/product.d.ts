b3b96fb36f19df3ed039718a28fb41cc29d54b22
ShirlaSoft DSI <yordicito2014@gmail.com>
2021-11-14 19:47:58 -0500

    x

--- packages/modelss/product/lib/product.d.ts
+++ packages/modelss/product/lib/product.d.ts
@@ -1,4 +1,5 @@
 export declare class Product {
     _id: string;
     name: string;
+    code: string;
 }
--- packages/modelss/product/src/product.ts
+++ packages/modelss/product/src/product.ts
@@ -1,5 +1,5 @@
 export class Product {
     _id:string;
     name:string;
-    
+    code:string;
 }
